92556188cae6fab0c45e5cbb3e4d1aa981d89503
JosSebastian <98227328+JosSebastian@users.noreply.github.com>
2023-05-28 10:08:44 -0500

    minor footer component ui restructuring

--- components/app/footer.vue
+++ components/app/footer.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="p-9">
+  <div class="px-9 pt-6 pb-3 bottom-0 left-0">
     <p class="text-sm text-indigo-700">
       <NuxtLink to="/"> Social Media </NuxtLink>
     </p>
