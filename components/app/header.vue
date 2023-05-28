8e6b42b2d859b06256dfc6f5d39b4cb8a6ab9802
JosSebastian <98227328+JosSebastian@users.noreply.github.com>
2023-05-27 22:33:52 -0500

    header component restructuring

--- components/app/header.vue
+++ components/app/header.vue
@@ -4,16 +4,20 @@ const search = ref("");
 
 <template>
   <div class="p-3 flex flex-row flex-wrap justify-around items-center gap-3">
+    <!-- Header Left -->
     <h3 class="text-3xl text-indigo-700">
       <NuxtLink to="/">Social Media</NuxtLink>
     </h3>
+    <!-- Header Right -->
     <div class="gap-3 flex flex-row items-center">
+      <!-- User Search -->
       <input
         v-model="search"
         type="text"
         placeholder="Search"
         class="px-2.5 py-1.5 caret-indigo-700 outline-none border focus:border-2 rounded border-indigo-300 focus:border-indigo-600"
       />
+      <!-- User Authentication -->
       <div class="flex flex-row gap-1">
         <Authentication v-bind:sign="false" />
         <Authentication v-bind:sign="true" />
