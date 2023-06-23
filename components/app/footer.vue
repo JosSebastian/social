35db347107fe177d4ce5d47f1ffedf3025df06bc
JosSebastian <98227328+JosSebastian@users.noreply.github.com>
2023-06-23 05:01:41 -0500

    minor header & footer ui changes

--- components/app/footer.vue
+++ components/app/footer.vue
@@ -2,9 +2,8 @@
 
 <template>
   <div class="px-9 pt-6 pb-3 bottom-0 left-0">
-    <!-- Footer -->
     <p class="text-sm text-indigo-700">
-      <NuxtLink to="/"> Social Media </NuxtLink>
+      <NuxtLink to="/">Social Media</NuxtLink>
     </p>
   </div>
 </template>
--- components/app/header.vue
+++ components/app/header.vue
@@ -8,20 +8,16 @@ const authenticate = (sign: string) => {
 
 <template>
   <div class="p-3 flex flex-row flex-wrap justify-around items-center gap-3">
-    <!-- Header Left -->
     <h3 class="text-3xl text-indigo-700">
       <NuxtLink to="/">Social Media</NuxtLink>
     </h3>
-    <!-- Header Right -->
     <div class="gap-3 flex flex-row items-center">
-      <!-- Search -->
       <input
         v-model="search"
         type="text"
         placeholder="Search"
         class="px-2.5 py-1.5 caret-indigo-700 outline-none border focus:border-2 rounded border-indigo-300 focus:border-indigo-600"
       />
-      <!-- Authentication -->
       <div v-if="!user" class="flex flex-row gap-1">
         <CustomButton v-on:click="authenticate('on')">Sign On</CustomButton>
         <CustomButton v-on:click="authenticate('in')">Sign In</CustomButton>
